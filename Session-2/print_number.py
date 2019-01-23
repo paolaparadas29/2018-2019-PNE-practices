a896d00583002641f456a67dc45a760d8dc6a003
paolaparadas29 <paolaparadas29@gmail.com>
2019-01-23 18:42:39 +0100

    The three last programs of the day

---
+++ Session-2/print_number.py
@@ -0,0 +1,4 @@
+print('The integer numbers from 1 to 20')
+
+for i in range(20):
+    print(i+1)
---
+++ Session-2/sum.py
@@ -0,0 +1,7 @@
+print('The sum of the first 100 integer numbers')
+total: int=0
+for i in range(100):
+    total=total+i+1
+
+print(total)
+
