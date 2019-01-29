bf60e1ec0cd40750427a1bfb6ed1e15f65a3a8ee
paolaparadas29 <paolaparadas29@gmail.com>
2019-01-29 15:42:01 +0100

    Exercises from the first two practices

---
+++ P0/P0/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    number1=0
+    number2=1#806515533049393
+    i=1
+    while i<n:
+        number1, number2= number2, number1+number2
+        i+=1
+    return number1
+
+number=int(input('Please introduce a n: '))
+nth_term= fibonacci(number)
+print('The n-th term of the fibonacci serie is: ', nth_term)
