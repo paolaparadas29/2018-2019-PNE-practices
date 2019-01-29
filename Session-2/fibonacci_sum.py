868f97a716fa6d1ba51a96e85df9bf557eb70eb7
paolaparadas29 <paolaparadas29@gmail.com>
2019-01-29 15:04:09 +0100

    A new version of fibonaccisum

--- Session-2/fibonacci_sum.py
+++ Session-2/fibonacci_sum.py
@@ -1,15 +1,17 @@
 def fib(n):
-    a, b = 0,1
-    sum=0
-    i=1
-    while i < n:
-        sum=sum+a
-        a, b = b, a+b
-        i+=1
-
-
-    return sum
+        sum=0
+        a, b = 0,1
+        i=1
+        while i < n:
+            sum=sum+a
+            a, b = b, a+b
+            i+=1
+        return sum
 
 number=int(input('Please introduce a n: '))
-sumnth_term= fib(number)
-print('The sum of the n-th term of the fibonacci serie is: ', sumnth_term)
+
+if number<1:
+    print('Please introduce a positive number')
+else:
+    sumnth_term= fib(number)
+    print('The sum of the n-th term of the fibonacci serie is: ', sumnth_term)
