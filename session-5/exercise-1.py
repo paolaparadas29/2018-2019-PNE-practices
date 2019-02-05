22cd7e69f81e0afd556abe7e611dbfbda72a85eb
paradas <paolaparadas29@gmail.com>
2019-02-05 16:48:58 +0100

    The first attempt to do exercise 1

---
+++ session-5/exercise-1.py
@@ -0,0 +1,38 @@
+def count_a(seq):
+    """Counting the number of As, CS, Ts and Gs in the sequence"""
+
+    # Counter for the As
+    resulta = 0
+    resultc = 0
+    resultt=0
+    resultg=0
+    for b in seq:
+
+        if b == 'A':
+            resulta += 1
+        elif b == 'C':
+            resultc += 1
+        elif b == 'T':
+            resultt += 1
+        elif b == 'G':
+            resultg += 1
+    # Return the result
+    return results= {'A': resulta, 'C': resultc, 'T': resultt, 'G': resultg}
+
+
+# Main program
+s = input('Enter the sequence: ')
+na = count_a(s)
+print('The number of As is:  {}'.format(na))
+
+# Calculate the total sequence lenght
+tl= len(s)
+
+#Calculate the percentage of As in the sequence
+if tl>0:
+    perc = round(100.0 * na/ tl,1)
+else:
+    perc=0
+
+print('This sequence is {} bases in lenght'.format(tl))
+print('The percentage of As is: {}%'.format(perc))
