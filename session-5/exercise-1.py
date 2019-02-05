d3e76161ad0edc5980e5dd668ce3377de8759b1f
paolaparadas29 <paolaparadas29@gmail.com>
2019-02-05 20:13:51 +0100

    Exercise 1 finished

--- session-5/exercise-1.py
+++ session-5/exercise-1.py
@@ -1,40 +1,46 @@
-def count_s(seq):
+def count_bases(seq):
     """Counting the number of As, CS, Ts and Gs in the sequence"""
 
     # Counter for the As
     resulta = 0
+    # Counter for the Cs
     resultc = 0
+    # Counter for te Ts
     resultt=0
+    # Counter for the Gs
     resultg=0
-    for b in seq:
 
-        if b == 'A':
+    for base in seq:
+
+        if base == 'A':
             resulta += 1
-        elif b == 'C':
+        elif base == 'C':
             resultc += 1
-        elif b == 'T':
+        elif base == 'T':
             resultt += 1
-        elif b == 'G':
+        elif base == 'G':
             resultg += 1
-    # Return the result
-    return results= {'A': resulta, 'C': resultc, 'T': resultt, 'G': resultg}
-
+          # Return the results
+    results= {'A': resulta, 'C': resultc, 'T': resultt, 'G': resultg}
+    return results
 
 
 
 # Main program
 s = input('Enter the sequence: ')
-n = count_s(s)
-print('The number of As is:  {}'.format(na))
-
+n = count_bases(s)
 # Calculate the total sequence lenght
 tl= len(s)
-
-#Calculate the percentage of As in the sequence
-if tl>0:
-    perc = round(100.0 * na/ tl,1)
-else:
-    perc=0
-
 print('This sequence is {} bases in lenght'.format(tl))
-print('The percentage of As is: {}%'.format(perc))
+
+for key in n:
+    # Calculate the percentage of As, Cs, Ts, Gs in the sequence
+    if tl > 0:
+        perc = round(100.0 * n[key] / tl, 1)
+    else:
+        perc = 0
+
+    # Results for As, Cs, Ts, Gs bases
+    print('Bases {}'.format(key))
+    print('  Counter: {}'.format(n[key]))
+    print('  Percentage: {}%'.format(perc))
