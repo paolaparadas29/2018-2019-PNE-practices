c4994458ff9180a7d4554fd8bf5f29dc490ce4a2
paolaparadas29 <paolaparadas29@gmail.com>
2019-02-06 20:00:07 +0100

    seq-main

---
+++ P1/Seq_main.py
@@ -0,0 +1,9 @@
+#Sequence 1: AGTACACTGGT
+ # Length: 11
+  #Bases count: A:3, T:3, C:2, G:3
+  #ases percentage: A:27.3%, T:27.3%, C:18.2%, G:27.3%
+
+from Seq import seq
+
+seq1= seq('ACTCT','A')
+print('Sequence 1:', r.self.strbases)
