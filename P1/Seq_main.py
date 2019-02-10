780cfa7061f4b4cdf9dc546016c634c68342fa8c
paolaparadas29 <paolaparadas29@gmail.com>
2019-02-10 22:54:17 +0100

    Class Seq_main (modified version)

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -1,49 +1,38 @@
 from Seq import Seq
 
 #Sequence 1
-seq1a = Seq('ACTCT' ,'A')
-seq1t = Seq('ACTCT','T')
-seq1c = Seq('ACTCT','C')
-seq1g = Seq('ACTCT','G')
+seq1 = Seq('ACTCT')
 
-print('Sequence 1:', seq1a.get_sequence())
-print('Lenght:', seq1a.len())
-print('Bases count:', 'A:', seq1a.count(), 'T:', seq1t.count(), 'C:', seq1c.count(), 'G:', seq1g.count())
-print('Bases percentage:', 'A:', seq1a.perc(),'%', 'T:', seq1t.perc(),'%', 'C:', seq1c.perc(),'%', 'G:', seq1g.perc(),'%')
+print('Sequence 1:', seq1.get_sequence())
+print('Lenght:', seq1.len())
+print('Bases count:', 'A:', seq1.count('A'), 'T:', seq1.count('T'), 'C:', seq1.count('C'), 'G:', seq1.count('G'))
+print('Bases percentage:', 'A:', seq1.perc('A'),'%', 'T:', seq1.perc('T'),'%', 'C:', seq1.perc('C'),'%', 'G:',seq1.perc('G'),'%')
 
 #Sequence 2
 
-seq2a= Seq('ACTCTTTTT','A')
-seq2t= Seq('ACTCTTTTT','T')
-seq2c= Seq('ACTCTTTTT','C')
-seq2g= Seq('ACTCTTTTT','G')
 
-print('Sequence 2:', seq2a.get_sequence())
-print('Lenght:', seq2a.len())
-print('Bases count:', 'A:', seq2a.count(), 'T:', seq2t.count(), 'C:', seq2c.count(), 'G:', seq2g.count())
-print('Bases percentage:', 'A:',seq2a.perc(),'%', 'T:',seq2t.perc(),'%', 'C:',seq2c.perc(),'%', 'G:',seq2g.perc(),'%')
+seq2= Seq('ACTCTTTTT')
+
+print('Sequence 2:', seq2.get_sequence())
+print('Lenght:', seq2.len())
+print('Bases count:', 'A:', seq2.count('A'), 'T:', seq2.count('T'), 'C:', seq2.count('C'), 'G:', seq2.count('G'))
+print('Bases percentage:', 'A:' ,seq2.perc('A'),'%', 'T:',seq2.perc('T'),'%', 'C:',seq2.perc('C'),'%', 'G:',seq2.perc('G'),'%')
 
 #Sequence 3
 
-seqc= seq1g.complement()
-seq3a= Seq(seqc,'A')
-seq3t= Seq(seqc,'T')
-seq3c= Seq(seqc,'C')
-seq3g= Seq(seqc,'G')
+seq3= seq1.complement()
 
-print('Sequence 3:', seqc)
-print('Lenght:', seq3a.len())
-print('Bases count:', 'A:', seq3a.count(), 'T:', seq3t.count(), 'C:', seq3c.count(), 'G:', seq3g.count())
-print('Bases percentage:', 'A:', seq3a.perc(),'%', 'T:', seq3t.perc(),'%', 'C:', seq3c.perc(),'%', 'G:', seq3g.perc(),'%')
+
+print('Sequence 3:', seq3)
+print('Lenght:', seq3.len())
+print('Bases count:', 'A:', seq3.count('A'), 'T:', seq3.count('T'), 'C:', seq3.count('C'), 'G:', seq3.count('G'))
+print('Bases percentage:', 'A:', seq3.perc('A'),'%', 'T:', seq3.perc('T'),'%', 'C:', seq3.perc('C'),'%', 'G:', seq3.perc('G'),'%')
 
 #Sequence 4
 
-seqr= seq1g.reverse()
-seq4a= Seq(seqr,'A')
-seq4t= Seq(seqr,'T')
-seq4c= Seq(seqr,'C')
-seq4g = Seq(seqr,'G')
-print('Sequence 4:', seqr)
-print('Lenght:', seq4a.len())
-print('Bases count:', 'A:', seq4a.count(), 'T:', seq4t.count(), 'C:', seq4c.count(), 'G:', seq4g.count())
-print('Bases percentage:', 'A:', seq4a.perc(),'%', 'T:', seq4t.perc(),'%', 'C:', seq4c.perc(),'%', 'G:', seq4g.perc(),'%')
+seq4= seq1.reverse()
+
+print('Sequence 4:', seq4)
+print('Lenght:', seq4.len())
+print('Bases count:', 'A:', seq4.count('A'), 'T:', seq4.count('T'), 'C:', seq4.count('C'), 'G:', seq4.count('G'))
+print('Bases percentage:', 'A:', seq4.perc('A'),'%', 'T:', seq4.perc('T'),'%', 'C:', seq4.perc('C'),'%', 'G:', seq4.perc('G'),'%')
