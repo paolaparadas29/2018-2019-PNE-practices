cdf9208f1c462d00d5a3bac75dd04098d3c82121
paradas <paolaparadas29@gmail.com>
2019-02-13 19:12:33 +0100

    Client of practice 2 (optional) modified

--- P2/client2.py
+++ P2/client2.py
@@ -1,18 +1,21 @@
+#Importing package socket
+
 import socket
 
-# Create  a socket for communicating with the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 PORT = 8080
 IP = "212.128.253.72"
-
-# Connect to the server
-s.connect((IP, PORT))
 
 while True:
 
     #Asking the user for a message
     sequence = input('Enter a sequence: ')
+
+    # Create  a socket for communicating with the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    s.connect((IP, PORT))
 
     # Sending message
     s.send(str.encode(sequence))
@@ -22,6 +25,4 @@ while True:
     print('MESSAGE FROM THE SERVER: ')
     print(msg)
 
-    # Closing the file
     s.close()
-
