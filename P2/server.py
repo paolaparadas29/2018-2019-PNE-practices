2e8fc46b7c6d3e810359c9ec27d92381efb5782e
paradas <paolaparadas29@gmail.com>
2019-02-13 18:55:34 +0100

    Server of practice 2

---
+++ P2/server.py
@@ -0,0 +1,48 @@
+#Importing package socket
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "212.128.253.72"
+MAX_OPEN_REQUESTS = 5
+
+# Counting the number of connections
+number_con = 0
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    serversocket.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = serversocket.accept()
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Message from client: {}".format(msg))
+
+        # Send the message
+        message = "The server received the reverse-complement of the sequence"
+        send_bytes = str.encode(message)
+
+        # Sending message to client
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
