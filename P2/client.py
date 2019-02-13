2ea116599c5a0743c80da9fe3ba819879b79abea
paradas <paolaparadas29@gmail.com>
2019-02-13 18:57:11 +0100

    Client of practice 2

---
+++ P2/client.py
@@ -0,0 +1,37 @@
+#importing package socket
+import socket
+
+#importing class Seq
+from Seq import Seq
+
+# Create  a socket for communicating with the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+PORT = 8080
+IP = "212.128.253.72"
+
+# Connect to the server
+s.connect((IP, PORT))
+
+while True:
+
+    #Asking the user for a message
+    sequence = input('Enter a sequence: ')
+    # Introducing the parameter 'sequence' to the object
+    s1= Seq(sequence)
+    #Reversing the sequence
+    s2=s1.reverse()
+    #Calculating the complement of the reverse
+    s3=s2.complement()
+
+    # Sending message
+    s.send(str.encode(s3.strbases))
+
+    #Receiving and decoding the message
+    msg = s.recv(2048).decode('utf-8')
+    print('MESSAGE FROM THE SERVER: ')
+    print(msg)
+
+    # Closing the file
+    s.close()
+
