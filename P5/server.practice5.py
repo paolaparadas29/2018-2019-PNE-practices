1e5d9d985e8dfc0208414c348162b7a8ab5cc17c
paradas <paolaparadas29@gmail.com>
2019-03-06 18:35:15 +0100

    Server of the practice 5

---
+++ P5/server.practice5.py
@@ -0,0 +1,68 @@
+import socketserver
+import termcolor
+import http.server
+
+
+PORT = 8006
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        #Selecting type
+
+        if self.path == '/':
+            contents = 'index.html'
+        elif self.path == '/blue':
+            contents = 'blue.html'
+        elif self.path == '/pink':
+            contents = 'pink.html'
+        else:
+            contents = 'error.html'
+
+        with open(contents, 'r') as a:
+            contents = a.read()
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# Main Program
+
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
